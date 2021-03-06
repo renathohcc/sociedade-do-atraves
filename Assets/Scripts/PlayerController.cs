22d4b205f066f3ba6f92bda489011ea053b2868b
Renatho Henrique Campos das Chagas <renathohcc@hotmail.com>
2021-03-06 16:21:49 -0300

    First Commit
    Test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movimentacao do player
+        theRB.velocity = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))* moveSpeed;
+    }
+}
