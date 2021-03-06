6a1f1976d3a211131d8cba2182f077f8390a861e
Renatho Henrique Campos das Chagas <renathohcc@hotmail.com>
2021-03-06 20:36:00 -0300

    Third Commit
    Added  Virtual Joystick

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D theRB;
     public float moveSpeed;
+    public FixedJoystick joystick;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Movimentacao do player
-        theRB.velocity = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))* moveSpeed;
+        // Movimentacao do player Setas
+        //theRB.velocity = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))* moveSpeed;
+
+        // Movimentacao do player Joystick Mobile
+        theRB.velocity = new Vector2 (joystick.Horizontal, joystick.Vertical)* moveSpeed;
     }
 }
